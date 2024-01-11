5bc23704362359ed69d9703982af1fcf488a2b90
FilippoBersan <filippobersan99@gmail.com>
2024-01-11 16:33:41 +0100

    feat: scaffolding

--- server.php
+++ server.php
@@ -1,3 +1,15 @@
 <?php
 
-?>
+
+// lettura file
+$database = file_get_contents(__DIR__ .'/dischi.json');
+
+
+
+// Struttura PHP
+$disks = json_decode($database);
+// var_dump($disks);
+
+
+header('Content-Type: application/json');
+echo json_encode($disks);
