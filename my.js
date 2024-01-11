066ccb91f70db6f6f2ebf0d4edd68d1e6057bb0c
FilippoBersan <filippobersan99@gmail.com>
2024-01-11 17:02:20 +0100

    feat: stampa dati

--- my.js
+++ my.js
@@ -3,38 +3,52 @@ const { createApp } = Vue;
 createApp({
   data() {
     return {
-      disks: [
-        {
-          name: 'New Jersey',
-          artist: 'Bon Jovi',
-          year: '1988',
-        },
-        {
-          name: 'Live at Wembley 86',
-          artist: 'Queen',
-          year: '1992',
-        },
-        {
-          name: "Ten's Tales",
-          artist: 'Sting',
-          year: '1993',
-        },
-        {
-          name: 'Steve Gadd Band',
-          artist: 'Steve Gadd Band',
-          year: '2018',
-        },
-        {
-          name: 'Brave New World',
-          artist: 'Iron maiden',
-          year: '2000',
-        },
-        {
-          name: 'One more car',
-          artist: 'Eric Clapton',
-          year: '2002',
-        },
-      ],
+      // disks: [
+      //   {
+      //     name: 'New Jersey',
+      //     artist: 'Bon Jovi',
+      //     year: '1988',
+      //   },
+      //   {
+      //     name: 'Live at Wembley 86',
+      //     artist: 'Queen',
+      //     year: '1992',
+      //   },
+      //   {
+      //     name: "Ten's Tales",
+      //     artist: 'Sting',
+      //     year: '1993',
+      //   },
+      //   {
+      //     name: 'Steve Gadd Band',
+      //     artist: 'Steve Gadd Band',
+      //     year: '2018',
+      //   },
+      //   {
+      //     name: 'Brave New World',
+      //     artist: 'Iron maiden',
+      //     year: '2000',
+      //   },
+      //   {
+      //     name: 'One more car',
+      //     artist: 'Eric Clapton',
+      //     year: '2002',
+      //     aa: 'ciao',
+      //   },
+      // ],
+
+      disks: [],
     };
+  },
+  methods: {
+    getDisks() {
+      axios.get('server.php').then((response) => {
+        console.log({ response });
+        this.disks = response.data;
+      });
+    },
+  },
+  created() {
+    this.getDisks();
   },
 }).mount('#app');
